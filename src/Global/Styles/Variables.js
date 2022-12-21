1c741f185c12ca758d8fb82136c98a6d72429b9d
iliyazCodes <Iliyaz.syed1995@gmail.com>
2022-12-22 00:39:29 +0530

    Header Basic Implementation

---
+++ src/Global/Styles/Variables.js
@@ -0,0 +1,21 @@
+import { css } from "styled-components"
+
+const variables = css`
+  :root {
+    --border-radius: 4px;
+    --transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
+    --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
+    --header-box-shadow: 0 3px 5px -1px #0003, 0 6px 10px #00000024, 0 1px 18px #0000001f;
+    --header-top-height: 100px;
+    --header-scroll-height: 70px;
+    --xs-space-size: 1em;
+    --md-space-size: 2em;
+    --lg-space-size: 3em;
+    --font-size-1: 1em;
+    --font-size-2: 2em;
+    --font-size-3: 3em;
+    --font-family: 'Poppins', sans-serif;
+  }
+`
+
+export default variables
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import GlobalStyles from "./components/global/Styled"
-import Theme from "./components/global/Theme"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <React.StrictMode>
-    <Theme>
-      <>
-        <GlobalStyles />
-        <App />
-      </>
-    </Theme>
-  </React.StrictMode>
-)
+root.render(<App />)
